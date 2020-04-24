db133c85c9aee511943d06c9323da3579ccb393e
Erick Martins Ratamero <erickmartins@gmx.net>
2020-04-24 16:47:45 -0400

    detected ROIs are being saved back to omero!

--- src/create_rois.py
+++ src/create_rois.py
@@ -1,23 +1,57 @@
-def create_rois(image, size_thresh, closing):
+def create_rois(image, size_thresh, method_thresh, closing):
     from skimage.transform import rescale, downscale_local_mean
-    from skimage.color import rgb2gray
-    from skimage.filters import threshold_otsu
+    from skimage.color import rgb2gray,label2rgb
+    from skimage.filters import threshold_otsu, threshold_triangle
     from skimage.util import invert
     from skimage.morphology import diamond, binary_closing
     from skimage.measure import regionprops, label
     import numpy as np
+    from skimage.io import imshow
+    import matplotlib.pyplot as plt
 
     im = rgb2gray(image)
     im = invert(im)
     
-    im_thresh = im > threshold_otsu(im)
+    if method_thresh == 'otsu':
+        im_thresh = im > threshold_otsu(im)
+    elif method_thresh == 'triangle':
+        im_thresh = im > threshold_triangle(im)
+    elif method_thresh == 'yen':
+        im_thresh = im > threshold_yen(im)
+    elif method_thresh == 'li':
+        im_thresh = im > threshold_li(im)
     im_thresh = binary_closing(im_thresh, diamond(closing))
     im_lab = label(im_thresh)
     for i in range(1, im_lab.max()+1):
         coords = np.where(im_lab == i)
         if len(coords[0]) < size_thresh:
             im_lab[coords] = 0
-        
+    im_lab_color = label2rgb(im_lab, bg_label = 0, bg_color=(0,0,0))
+    # imshow(im_lab_color)
+    # plt.show()    
     regions = regionprops(im_lab)
     return regions
 
+
+
+if __name__ == "__main__":
+    import os
+    from create_session import create_json_session, create_blitz_session
+    from retrieve_image import retrieve_image
+    from save_rois import save_rois, get_image
+    
+
+
+    WEB_HOSTNAME = os.environ['OMERO_WEB_HOSTNAME']
+    HOSTNAME = os.environ['OMERO_HOSTNAME']
+    USERNAME = os.environ['OMERO_ADMIN_USER']
+    PASSWORD = os.environ['OMERO_ADMIN_PASSWORD']
+    img_id = 1
+    scale_factor = 64
+    login_rsp, session, base_url = create_json_session(WEB_HOSTNAME, USERNAME, PASSWORD)
+    img = retrieve_image(session, base_url, img_id, scale_factor)
+    regions = create_rois(img, 200, 'triangle', 5)
+    conn = create_blitz_session(HOSTNAME, USERNAME, PASSWORD)
+    image = get_image(conn, img_id)
+    save_rois(image, regions, scale_factor)
+    conn.close()
---
+++ src/save_rois.py
@@ -0,0 +1,83 @@
+from omero.rtypes import rdouble, rint, rstring
+
+def save_rois(image, regions, scale):
+    """
+    Usage: In napari, open console...
+    >>> from omero_napari import *
+    >>> save_rois(viewer, omero_image)
+    """
+    if regions and image:
+        conn = image._conn
+        counter = 1
+        for region in regions:
+            bbox = region.bbox
+            
+            shape = create_rectangle(bbox, image, counter, scale)
+            if shape is not None:
+                roi = create_roi(conn, image, [shape])
+            counter = counter + 1
+            
+    else:
+        return None
+    
+
+
+def get_t(coordinate, image):
+    if image.getSizeT() > 1:
+        return coordinate[0]
+    return 0
+
+def get_z(coordinate, image):
+    if image.getSizeZ() == 1:
+        return 0
+    if image.getSizeT() == 1:
+        return coordinate[0]
+    #if coordinate includes T and Z... [t, z, x, y]
+    return coordinate[1]
+
+
+def create_rectangle(data, image, order, scale):
+    
+    from omero.model import RectangleI
+    z_index = get_z(0, image)
+    t_index = get_t(0, image)
+    y1 = data[0] * scale
+    x1 = data[1] * scale
+    h = (data[2] - data[0]) * scale
+    w = (data[3] - data[1]) * scale
+    
+    shape = RectangleI()
+    # TODO: handle 'updside down' rectangle x3 < x1
+    shape.x = rdouble(x1)
+    shape.y = rdouble(y1)
+    shape.width = rdouble(w)
+    shape.height = rdouble(h)
+    
+    shape.textValue = rstring("ROI "+str(order))
+     
+
+    if shape is not None:
+        shape.theZ = rint(z_index)
+        shape.theT = rint(t_index)
+    return shape
+
+def create_roi(conn, img, shapes):
+    from omero.model import RoiI
+    updateService = conn.getUpdateService()
+    roi = RoiI()
+    #img = get_image(conn, img_id)
+    roi.setImage(img._obj)
+    for shape in shapes:
+        roi.addShape(shape)
+    group_id = img.getDetails().getGroup().getId()
+    ctx = {'omero.group': str(group_id)}
+    return updateService.saveAndReturnObject(roi, ctx)
+
+
+def get_image(conn, image_id):
+    
+    if (image_id):
+        image = conn.getObject("Image", image_id)
+        return image
+    else:
+        return None
